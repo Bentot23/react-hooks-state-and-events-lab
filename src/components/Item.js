38a3d16c4875a7c1f994472c67f38da33f2832a8
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-09-14 12:21:50 -0500

    first commit in this lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isClick, setIsClick] = useState(false)
+
+  function handleClick() {
+    setIsClick((isClick) => !isClick)
+  }
+  const color = isClick ? "pink" : "yellow";
+  const itemClass = isClick ? "in-cart" : "";
+
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" style={{ background: color }}onClick={handleClick}>{isClick ? 'Remove From Cart' : 'Add to Cart'}</button>
     </li>
   );
 }
